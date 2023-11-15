7b489d19692aedfda3084cc714202ef12582a59d
Daniel Zeitler <dz@danielzeitler.de>
2023-11-15 09:37:38 +0100

    change to POST

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,4 +1,4 @@
-export async function GET(request: Request) {
+export async function POST(request: Request) {
     console.log(request.body)
     return Response.json({ message: 'Hello world' });
 }
