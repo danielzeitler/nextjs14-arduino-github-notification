58ff65784e26f2797029e032964fe732b2d6e9f8
Daniel Zeitler <dz@danielzeitler.de>
2023-11-15 10:19:05 +0100

    test commit

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,7 +1,9 @@
 export async function POST(req: any) {
     const data = await req.json();
     console.log("*******")
-    console.log(data)
+    console.log(data.context)
+    console.log(data.description)
+    console.log(data.state)
     console.log("*******")
     return Response.json({message: 'Hello world'});
 }
