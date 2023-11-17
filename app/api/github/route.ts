1875ad26fb82a3700596764ff03324f876f98460
Daniel Zeitler <dz@danielzeitler.de>
2023-11-17 10:18:00 +0100

    build

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: any) {
 
     client.on('connect', function () {
         if(data.starred_at) {
-            console.log(`process: ${data.context}, ${data.description}, ${data.state}`);
+            console.log(`${data.context}, ${data.description}, ${data.state}`);
             client.publish('test/event', `${data.context}, ${data.description}, ${data.state}`);
         }
     });
